f7a47a91c4e3af37101f22aca3d3a7d0c670783a
Joao P Ferreira Lucas <joaopferreiralucas@gmail.com>
2023-02-08 21:29:28 -0300

    types for posts created

---
+++ .babelrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "presets": ["next/babell"],
+  "plugins": [["styled-components", {"ssr": true}]]
+}
---
+++ src/styles/global-styles.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+`;
